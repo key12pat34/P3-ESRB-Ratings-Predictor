3d0e91c5cb332df6d988e7b0bc564bca5b4af0b2
sydrob10 <sydrobertson10@gmail.com>
2021-02-25 21:32:23 -0500

    html updates

--- webpage/app.py
+++ webpage/app.py
@@ -18,18 +18,18 @@ import numpy as np
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("postgres://bwrtaugijyfgyd:2a00951144d14d957fe21c02613f65b2083e6f64f7cc32c28d784c5e8b8960dc@ec2-54-205-187-125.compute-1.amazonaws.com:5432/d8uhbccr3c2pvb")
+# engine = create_engine("postgres://bwrtaugijyfgyd:2a00951144d14d957fe21c02613f65b2083e6f64f7cc32c28d784c5e8b8960dc@ec2-54-205-187-125.compute-1.amazonaws.com:5432/d8uhbccr3c2pvb")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-School = Base.classes.schooltable
+# # Save reference to the table
+# School = Base.classes.schooltable
 
-# Marker Table
-Marker = Base.classes.marker_data
+# # Marker Table
+# Marker = Base.classes.marker_data
 
 #################################################
 # Flask Setup
@@ -41,32 +41,17 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def homepage():
     return render_template("home.html")
 
-# Query the database and send the jsonified results
-@app.route("/map.html")
-def map():
-    return render_template("map.html")
+@app.route("/analysis.html")
+def analysis():
+    return render_template("analysis.html")
 
-@app.route("/api")
-def location():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    results = session.query(Marker.name, Marker.enrollment, Marker.teachercount, Marker.schooltype, Marker.latitude, Marker.longitude).all()
-    # results = session.query(School._id, School.name).all()
-    session.close()
-    
-    # Convert list of tuples into normal lists
-    all_schools = []
-    
-    for name, enrollment, teachercount, schooltype, latitude, longitude in results:
-        school_dict = {}
-        school_dict["name"] = name
-        school_dict["enrollment"] = enrollment
-        school_dict["teachercount"] = teachercount
-        school_dict["type"] = schooltype
-        school_dict["latitude"] = latitude
-        school_dict["longitude"] = longitude
-        all_schools.append(school_dict)
-    return jsonify(all_schools)
+@app.route("/predictor.html")
+def predict():
+    return render_template("predictor.html")
+
+@app.route("/walkthrough.html")
+def walkthrough():
+    return render_template("walkthrough.html")
 
 if __name__ == "__main__":
     app.run()
